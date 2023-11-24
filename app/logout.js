91c8129d9503dddb4c21a561c2c68528dd6c9d12
eliasib <ecantor.2906@gmail.com>
2023-11-24 10:07:31 -0500

    4 commit

--- app/logout.js
+++
@@ -1,9 +0,0 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
-import { auth } from './firebase.js'
-
-const logout = document.getElementById("logout")
-
-logout.addEventListener('click',  async () => {
-    await signOut(auth)
-    console.log('User signed out ')
-})
